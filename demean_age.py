dbcaed917674855fcf39722ddb62545fdf2b36f1
Scott Peltier <spelt@umich.edu>
2017-09-04 14:51:31 -0700

    test

--- demean_age.py
+++ demean_age.py
@@ -6,4 +6,9 @@ mean_age = sum(age)/len(age)
 
 np.savetxt("demeaned_age.txt", age-mean_age)
 
+assert mean_age < 100
+assert mean_age > 10 
+
+
+
 print("done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
